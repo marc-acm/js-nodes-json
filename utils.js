2d54a5d5e24511b678591db0c8b26848c274481f
marc-acm <marc.macariola@yaho.com>
2017-08-31 15:42:31 +0800

    first commit

---
+++ utils.js
@@ -0,0 +1,8 @@
+
+function jsonResponse(res, json) {
+	const jsonString = JSON.stringify(json);
+	res.setHeader('Content-Type', 'application/json');
+	res.end(jsonString);
+}
+
+module.exports = { jsonResponse: jsonResponse};
